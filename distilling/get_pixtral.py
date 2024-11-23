ea964ea74390b3542a108fc2bede26af6ba3cca6
Francesco Capuano <francesco_capuano@aol.com>
2024-11-23 08:22:09 +0100

    remove: useless code

--- distilling/get_pixtral.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoProcessor, LlavaForConditionalGeneration
-model_id = "mistral-community/pixtral-12b"
-model = LlavaForConditionalGeneration.from_pretrained(model_id)
-processor = AutoProcessor.from_pretrained(model_id)
-
